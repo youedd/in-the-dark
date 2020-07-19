f453f7a1a5604a03a16b72f92b2faed57d0f4d91
Youssef Eddibili <eddibili.youssef@gmail.com>
2020-07-19 18:51:55 +0200

    fix debeug conditions

--- src/debugger.ts
+++ src/debugger.ts
@@ -3,22 +3,23 @@ interface Window {
 }
 
 export class Debugger {
-    public static _isLocked = true;
+    public static isLocked = true;
     static isDebug = false;
 
     static on = () => Debugger.isDebug = true;
 
     static off = () => Debugger.isDebug = false;
 
-    static next = () => Debugger._isLocked = false;
+    static next = () => Debugger.isLocked = false;
 
-    static isLocked = () => Debugger.isDebug && Debugger._isLocked;
-
-    static lock = () => Debugger._isLocked = true;
+    static lock = () => Debugger.isLocked = true;
 
     static debug = (...args: any) => {
-        if (!Debugger.isLocked()) return
-        console.log(args)
+        if (!Debugger.isDebug) return
+
+        if (Debugger.isLocked) return
+
+        console.log(args);
     }
 }
 
